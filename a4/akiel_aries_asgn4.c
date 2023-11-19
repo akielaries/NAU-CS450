1b679e29807d469bbc97875bf9272d4abe9f5059
akielaries <akiel@akiel.org>
2023-11-19 13:31:20 -0700

    adding try at a4

--- a4/akiel_aries_asgn4.c
+++ a4/akiel_aries_asgn4.c
@@ -4,43 +4,40 @@
 #include <sys/time.h>
 #include <time.h>
 
-
 double function_to_parallelize(int N, int M) {
     long i, j;
     long A = 0;
     double B = 0, C = 0, D = 0;
 
-
+    #pragma omp parallel for private(i, j) reduction(+:A) schedule(static)
     for (i = 0; i < N; i++) {
         for (j = 0; j < M; j++) {
             A += i * j;
-            //printf("%ld * %ld A += %ld\n", i, j, A);
         }
     }
 
+    #pragma omp parallel for reduction(+:B)
     for (i = 1; i < (long)sqrt(A); i++) {
         B += 1 / i;
-        //printf("1 / %ld B += %f\n", i, B);
     }
 
-    for (i = 0; i < M * N; i++)
+    #pragma omp parallel for private(i, j) reduction(+:D) schedule(static)
+    for (i = 0; i < M * N; i++) {
         for (j = 0; j < M; j++) {
             D += pow(0.1, i * j);
-            //printf("D += %lf\n", D);
         }
+    }
 
-    #pragma omp parallel for reduction(+:C)
-    for (i = 0; i < (long)B * (N + 1); i++)
+    #pragma omp parallel for private(i, j) reduction(+:C) schedule(static)
+    for (i = 0; i < (long)B * (N + 1); i++) {
         for (j = 1; j < (long)sqrt(D); j++) {
             C += i / j;
-            //printf("C += %lf\n", C);
         }
+    }
 
     double result = A + B - C / D;
-    printf("Result: %ld + %f - %f / %f = %lf\n", A, B, C, D, result);
+    printf("Result: %ld + %lf - %lf / %lf = %lf\n", A, B, C, D, result);
     return result;
-
-    // return A + B - C / D;
 }
 
 int main() {
